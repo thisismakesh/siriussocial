1734322dc4b1c2bd2f0a1231d3143125e8fc204f
VIGNESHPRABUT44\User <balajisurya493@gmail.com>
2017-12-20 12:40:28 +0530

    constants 2

--- nodejs/utils/constants.js
+++ nodejs/utils/constants.js
@@ -1,12 +1,12 @@
 module.exports ={
 		"MONGODBURL" : "mongodb://balaji:balaji@ds161136.mlab.com:61136/socsite",
+		"USER_HOMEPAGE_URL" : "http://localhost:8080/adPost",
 		"USER_SIGNUP_URL" : "http://localhost:8080/signUp",
+		"USER_LOGIN_URL" : "http://localhost:8080/logIn"
+		"USER_PROFILE_SETTINGS_PAGE_URL" : "http://localhost:8080/saveSettings",
 		"USER_SIGNUP_SUBMIT_URL" : "/SignUpUser",
 		"USER_SIGNUP_AJAX" : "http://localhost:8080/SignUpUser",
-		"USER_LOGIN_URL" : "http://localhost:8080/logIn"
 		"USER_LOGIN_SUBMIT_URL" : "/LogInUser",
 		"USER_LOGIN_AJAX" : "http://localhost:8080/LogInUser",
-		"USER_PROFILE_SETTINGS_PAGE_URL" : "http://localhost:8080/saveSettings",
-		"USER_SAVE_PROFILE_SETTINGS_AJAX" : "http://localhost:8080/saveProfileSettings"
-		"USER_HOMEPAGE_URL" : 'http://localhost:8080/adPost'	
+		"USER_SAVE_PROFILE_SETTINGS_AJAX" : "http://localhost:8080/saveProfileSettings"	
 }
